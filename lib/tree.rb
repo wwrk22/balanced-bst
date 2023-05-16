866901b9c7984bbc0197a83d8a558b25289ff2dd
Won Rhim <wwrk22@gmail.com>
2023-05-16 00:26:11 -0400

    Add some helpers

--- lib/tree.rb
+++ lib/tree.rb
@@ -5,9 +5,33 @@ class Tree
   end
 
   def set_root
-    if @array.size > 0
-      start_index, end_index = 0, @array.size - 1
-      @root = Node.new(@array[(start_index + end_index) / 2])
-    end
+    @root = Node.new(@array[mid_index(@array)]) if @array.size > 0
+  end
+
+  def build_tree
+    curr_root = @array[mid_index(@array)]
+    
+  end
+
+  private
+
+  # Return the middle index of the given array.
+  # Return -1 if the array is empty.
+  def mid_index(array)
+    return (0 + array.length - 1) / 2
+  end
+
+  def pp_right_args(node, prefix, is_left)
+    [node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false]
+  end
+
+  def pp_left_args(node, prefix, is_left)
+    [node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true]
+  end
+
+  def pretty_print(node = @root, prefix = '', is_left = true)
+    pretty_print(pp_right_args(node, prefix, is_left)) if node.right
+    puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
+    pretty_print(pp_left_args(node, prefix, is_left) if node.left
   end
 end
