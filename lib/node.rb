09a78e52ed14593bdf31d56abbe46491ebc5a2ec
Won Rhim <wwrk22@gmail.com>
2023-05-15 23:59:31 -0400

    Add node

---
+++ lib/node.rb
@@ -0,0 +1,13 @@
+class Node
+  include Comparable
+
+  attr_accessor :left_child, :right_child
+
+  def initialize(data)
+    @data = data
+  end
+
+  def <=>(other_node)
+    @data <=> other_node.data
+  end
+end
